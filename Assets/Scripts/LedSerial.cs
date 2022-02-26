fcd0b49700f2eaffcdb2c4fe7680f12a9ee8d1e6
xpeng <1216772231@qq.com>
2022-02-26 21:16:34 +0100

    v0.6.2 fix, rewritten read serial function (LED)

--- Assets/Scripts/LedSerial.cs
+++ Assets/Scripts/LedSerial.cs
@@ -1,22 +1,21 @@
-
+﻿
 using UnityEngine;
 using System.IO.Ports;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LedSerial : MonoBehaviour
 {
     static SerialPort p1Serial = new SerialPort ("COM51", 115200);
     // Start is called before the first frame update
-    byte[] dataPacket = new byte[13];
-    byte[] incomPacket = new byte[13];
-    float timer = 0; 
+    public List<byte> dataPacket = new List<byte>();
+    public List<byte> incomPacket = new List<byte>();
     byte recivData;
-    int packLeng = 0;
     public Light[] Lights;
     public float LightIntensity = 3f;
     Color32 PrevFadeColor;
-    bool headState = false;
+    Color32 nowCorlor;
     void Start()
     {
         p1Serial.Open();
@@ -26,17 +25,11 @@ public class LedSerial : MonoBehaviour
     {
         //ReadPack();
         ReadData();
+        UpdateLED();
         StartCoroutine(FixLedPower());
     }
-    IEnumerator FixLedPower()
-    {
-        for (int i = 0; i < 8; i++)
-        {
-            Lights[i].intensity = (LightIntensity / (Lights[i].color.r + Lights[i].color.g + Lights[i].color.b)) +0.1f;
-            yield return null;
-        }
-    }
-    private void ReadPack()
+    
+    void ReadPack()
     {
         ReadData();
         Debug.Log("RX: "+dataPacket[0]+"-"+
@@ -51,59 +44,50 @@ public class LedSerial : MonoBehaviour
                                     dataPacket[9]+"-"+
                                     dataPacket[10]);
     }
-    private void ReadData()
+    void ReadData()
     {
-        timer = 0f;
-        if (p1Serial.BytesToRead >= 3)
+        while (p1Serial.BytesToRead > 0)
         {
-            if (!headState)
-                recivData = Convert.ToByte(p1Serial.ReadByte());
-            headState = false;
-            if (recivData == 224) 
+            recivData = Convert.ToByte(p1Serial.ReadByte());
+            if (recivData == 224)
             {
-                do
-                {
-                    incomPacket[packLeng++] = recivData;
-                    recivData = Convert.ToByte(p1Serial.ReadByte());
-                }
-                while (recivData != 224 & p1Serial.BytesToRead >= 1);
-                headState = true;
-                dataPacket = incomPacket;
-                packLeng = 0;
-                UpdateLED();
+                dataPacket = new List<byte>(incomPacket);
+                incomPacket.Clear();
+                incomPacket.Add(recivData);
+                break;
             }
+            incomPacket.Add(recivData);
         }
+            
     }
 
-    private void UpdateLED()
+    void UpdateLED()
     {
+        if (dataPacket.Count < 9)
+            return;
         switch (dataPacket[4])
         { 
             case 49:
                 if (dataPacket[5] > 7)
                     dataPacket[5] = 7;
                 Lights[dataPacket[5]].color = new Color32(dataPacket[6], dataPacket[7], dataPacket[8], 255);
+                dataPacket.Clear();
                 break;
             case 50:
-                if (dataPacket[6] > 8)
-                    dataPacket[6] = 8;
-                for (int i = dataPacket[5]; i < dataPacket[6]; i++)
-                {
-                    Lights[i].color = new Color32(dataPacket[8], dataPacket[9], dataPacket[10], 255);
-                }
-                PrevFadeColor = new Color32(dataPacket[8], dataPacket[9], dataPacket[10], 255);
-                break;
             case 51:
                 if (dataPacket[6] > 8)
                     dataPacket[6] = 8;
-                Color32 nowCorlor = new Color32(dataPacket[8], dataPacket[9], dataPacket[10], 255);
-                StartCoroutine(Fade(dataPacket[5], dataPacket[6], Lights,PrevFadeColor, nowCorlor, dataPacket[11]));
+                nowCorlor = new Color32(dataPacket[8], dataPacket[9], dataPacket[10], 255);
+                if (dataPacket[4]==50)
+                    StartCoroutine(Switch(dataPacket[5], dataPacket[6], Lights, nowCorlor));
+                else
+                    StartCoroutine(Fade(dataPacket[5], dataPacket[6], Lights,PrevFadeColor, nowCorlor, dataPacket[11]));
                 PrevFadeColor = new Color32(dataPacket[8], dataPacket[9], dataPacket[10], 255);
-                //Debug.Log("Fade");
+                dataPacket.Clear();
                 break;
         }
     }
-    private IEnumerator Fade(byte start, byte end, Light[] Lights, Color32 prevColor, Color32 nowColor, float duration)
+    IEnumerator Fade(byte start, byte end, Light[] Lights, Color32 prevColor, Color32 nowColor, float duration)
     {
         duration = 4095 / duration * 8 / 1000;
         //Debug.Log(duration);
@@ -113,7 +97,24 @@ public class LedSerial : MonoBehaviour
             for (int i = start; i < end; i++)
             {
                 Lights[i].color = Color.Lerp(prevColor, nowColor, progress);
+                //yield return null;
             }
+            yield return null;
+        }
+    }
+    IEnumerator Switch(byte start, byte end, Light[] Lights, Color32 Color)
+    {
+        for (int i = start; i < end; i++)
+        {
+            Lights[i].color = Color;
+            yield return null;
+        }
+    }
+    IEnumerator FixLedPower()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            Lights[i].intensity = (LightIntensity / (Lights[i].color.r + Lights[i].color.g + Lights[i].color.b)) +0.1f;
             yield return null;
         }
     }
