b2e72a98ea714e8117aefc399d3b6f59670c90fc
xpeng <1216772231@qq.com>
2022-03-04 04:20:56 +0100

    small adjust

--- Assets/Scripts/LedSerial.cs
+++ Assets/Scripts/LedSerial.cs
@@ -26,7 +26,7 @@ public class LedSerial : MonoBehaviour
         //ReadPack();
         ReadData();
         UpdateLED();
-        StartCoroutine(FixLedPower());
+        FixLedPower();
     }
     
     void ReadPack()
@@ -54,7 +54,7 @@ public class LedSerial : MonoBehaviour
                 dataPacket = new List<byte>(incomPacket);
                 incomPacket.Clear();
                 incomPacket.Add(recivData);
-                break;
+                return;
             }
             incomPacket.Add(recivData);
         }
@@ -79,7 +79,7 @@ public class LedSerial : MonoBehaviour
                     dataPacket[6] = 8;
                 nowCorlor = new Color32(dataPacket[8], dataPacket[9], dataPacket[10], 255);
                 if (dataPacket[4]==50)
-                    StartCoroutine(Switch(dataPacket[5], dataPacket[6], Lights, nowCorlor));
+                    Switch(dataPacket[5], dataPacket[6], Lights, nowCorlor);
                 else
                     StartCoroutine(Fade(dataPacket[5], dataPacket[6], Lights,PrevFadeColor, nowCorlor, dataPacket[11]));
                 PrevFadeColor = new Color32(dataPacket[8], dataPacket[9], dataPacket[10], 255);
@@ -102,20 +102,18 @@ public class LedSerial : MonoBehaviour
             yield return null;
         }
     }
-    IEnumerator Switch(byte start, byte end, Light[] Lights, Color32 Color)
+    void Switch(byte start, byte end, Light[] Lights, Color32 Color)
     {
         for (int i = start; i < end; i++)
         {
             Lights[i].color = Color;
-            yield return null;
         }
     }
-    IEnumerator FixLedPower()
+    void FixLedPower()
     {
         for (int i = 0; i < 8; i++)
         {
-            Lights[i].intensity = (LightIntensity / (Lights[i].color.r + Lights[i].color.g + Lights[i].color.b)) +0.1f;
-            yield return null;
+            Lights[i].intensity = LightIntensity / (Lights[i].color.r + Lights[i].color.g + Lights[i].color.b + 0.1f) ;
         }
     }
 }
