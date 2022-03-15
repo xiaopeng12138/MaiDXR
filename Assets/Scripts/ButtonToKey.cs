ddf474e81c24d964b4d7b89c1b634a578c7c0f2e
xpeng <1216772231@qq.com>
2022-03-15 18:51:21 +0100

    Fix touch and button issue (based on #16)

--- Assets/Scripts/ButtonToKey.cs
+++ Assets/Scripts/ButtonToKey.cs
@@ -11,28 +11,18 @@ public class ButtonToKey : MonoBehaviour
     public VirtualKeyCode keyToPress;
 
     private int _insideColliderCount = 0;
-
-    private void Update()
-    {
-        if (_insideColliderCount == 0)
-        {
-            keybd_event(System.Convert.ToByte(keyToPress), (byte)MapVirtualKey((uint)keyToPress, 0), 2, UIntPtr.Zero);
-        }
-        else
-        {
-            keybd_event(System.Convert.ToByte(keyToPress), (byte)MapVirtualKey((uint)keyToPress, 0), 0, UIntPtr.Zero);  
-        }
-    }
-
+    private bool triggered = true;
     private void OnTriggerEnter(Collider other)
     {
+        keybd_event(System.Convert.ToByte(keyToPress), (byte)MapVirtualKey((uint)keyToPress, 0), 0, UIntPtr.Zero);
         _insideColliderCount += 1;
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (_insideColliderCount == 0)
+            keybd_event(System.Convert.ToByte(keyToPress), (byte)MapVirtualKey((uint)keyToPress, 0), 2, UIntPtr.Zero);
         _insideColliderCount -= 1;
         _insideColliderCount = Mathf.Max(0, _insideColliderCount);
     }
-    
 }
--- Assets/Scripts/TouchToSerial.cs
+++ Assets/Scripts/TouchToSerial.cs
@@ -3,29 +3,21 @@ using UnityEngine;
 public class TouchToSerial : MonoBehaviour
 {
     public int Area;
-    int _insideColliderCount = 0;
-
-    private void Update()
-    {
-        if (_insideColliderCount == 0)
-        {
-            Serial.ChangeTouch((int)Area, true);
-            Serial.SendTouch();
-        }
-        else
-        {
-            Serial.ChangeTouch((int)Area, false);
-            Serial.SendTouch();
-        }
-    }
-    
+    private int _insideColliderCount = 0;
     private void OnTriggerEnter(Collider other)
     {
+        Serial.ChangeTouch((int)Area, true);
+        Serial.SendTouch();
         _insideColliderCount += 1;
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (_insideColliderCount == 0)
+        {
+            Serial.ChangeTouch((int)Area, false);
+            Serial.SendTouch();
+        }
         _insideColliderCount -= 1;
         _insideColliderCount = Mathf.Max(0, _insideColliderCount);
     }
