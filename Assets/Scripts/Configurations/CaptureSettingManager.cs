f860005057f50cc899e7c8cfb6f9b335c132bf38
xpeng <1216772231@qq.com>
2022-08-17 18:37:55 +0200

    add capture setting manager

---
+++ Assets/Scripts/Configurations/CaptureSettingManager.cs
@@ -0,0 +1,104 @@
+using TMPro;
+using UnityEngine;
+using uWindowCapture;
+using UnityEngine.UI;
+
+public class CaptureSettingManager : MonoBehaviour
+{
+    public GameObject CaptureDisplay;
+    private UwcWindowTexture WindowTexture;
+    private Material WindowMaterial;
+    private TMP_Dropdown Dropdown;
+    private Toggle Toggle;
+    void Start()
+    {
+        WindowTexture = CaptureDisplay.GetComponent<UwcWindowTexture>();
+        WindowMaterial = CaptureDisplay.GetComponent<Renderer>().material;
+        Dropdown = GetComponent<TMP_Dropdown>();
+        Toggle = GetComponent<Toggle>();
+        switch (gameObject.name)
+        {
+            case "CPModeDropdown":
+                GetCPMode();
+                break;
+            case "CPDesktop":
+                GetCPDesktop();
+                break;
+            case "CPFPSDropdown":
+                GetCPFPS();
+                break;
+            case "CP1P":
+                GetCP1P();
+                break;
+        }
+    }
+    public void GetCPMode()
+    {
+        if (JsonConfig.HasKey("CaptureMode")) 
+            Dropdown.value = JsonConfig.GetInt("CaptureMode");
+        SetCPMode();
+    }
+    public void GetCPDesktop()
+    {
+        if (JsonConfig.HasKey("CaptureDesktop")) 
+            Toggle.isOn = JsonConfig.GetBoolean("CaptureDesktop");
+        SetCPDesktop();
+    }
+    public void GetCPFPS()
+    {
+        if (JsonConfig.HasKey("CaptureFPS")) 
+            Dropdown.value = JsonConfig.GetInt("CaptureFPS");
+        SetCPFPS();
+
+    }
+    public void GetCP1P()
+    {
+        if (JsonConfig.HasKey("Capture1P")) 
+            Toggle.isOn = JsonConfig.GetBoolean("Capture1P");
+        SetCP1P();
+    }
+
+    public void SetCPMode()
+    {
+        WindowTexture.captureMode = (CaptureMode)Dropdown.value;
+        JsonConfig.SetInt("CaptureMode", Dropdown.value);
+    }
+    
+    public void SetCPDesktop()
+    {
+        if (Toggle.isOn)
+            WindowTexture.type = WindowTextureType.Desktop;
+        else
+            WindowTexture.type = WindowTextureType.Window;
+        JsonConfig.SetBoolean("CaptureDesktop", Toggle.isOn);
+    }
+    
+    public void SetCPFPS()
+    {
+        switch (Dropdown.value)
+        {
+            case 0:
+                WindowTexture.captureFrameRate = 30;
+                break;
+            case 1:
+                WindowTexture.captureFrameRate = 60;
+                break; 
+            case 2:
+                WindowTexture.captureFrameRate = 90;
+                break;
+            case 3:
+                WindowTexture.captureFrameRate = 120;
+                break;
+            case 4:
+                WindowTexture.captureFrameRate = 144;
+                break;
+        }
+        JsonConfig.SetInt("CaptureFPS", Dropdown.value);
+    }
+    
+    public void SetCP1P()
+    {
+        WindowMaterial.SetTextureScale("_MainTex",new Vector2(Toggle.isOn ? 1f : 0.5f, 1));
+        JsonConfig.SetBoolean("Capture1P", Toggle.isOn);
+    }
+}
---
+++ Assets/Scripts/Configurations/JsonConfig.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Linq;
+
+public static class JsonConfig {
+    public static bool hasInitialized = false;
+    private static JObject config;
+
+    private static void ensureInitialization() {
+        if (hasInitialized) 
+            return;
+        loadFile();
+        hasInitialized = true;
+    }
+
+    private static string getFileName() {
+        return Application.dataPath + "/../config.json";
+    }
+
+    private static void saveFile() {
+        File.WriteAllText(getFileName(), config.ToString());
+    }
+
+    private static void loadFile() {
+        if (File.Exists(getFileName())) 
+            config = JObject.Parse(File.ReadAllText(getFileName()));
+        else {
+            config = new JObject();
+            saveFile();
+        }
+    }
+
+    public static void DeleteAll() {
+        ensureInitialization();
+        config.RemoveAll();
+        saveFile();
+    }
+    public static void DeleteKey(string key) {
+        ensureInitialization();
+        config.Remove(key);
+        saveFile();
+    }
+
+    public static bool HasKey(string key) {
+        ensureInitialization();
+        return config.ContainsKey(key);
+    }
+
+    public static void SetBoolean(string key, bool boolean) {
+        ensureInitialization();
+        config[key] = boolean;
+        saveFile();
+    }
+    public static void SetString(string key, string text) {
+        ensureInitialization();
+        config[key] = text;
+        saveFile();
+    }
+    public static void SetInt(string key, int number) {
+        ensureInitialization();
+        config[key] = number;
+        saveFile();
+    }
+    public static void SetDouble(string key, double number) {
+        ensureInitialization();
+        config[key] = number;
+        saveFile();
+    }
+    public static void SetFloatArray(string key, float[] numbers) {
+        ensureInitialization();
+        config[key] = JArray.FromObject(numbers);
+        saveFile();
+    }
+
+    public static bool GetBoolean(string key) {
+        ensureInitialization();
+        return config.Value<bool>(key);
+    }
+    public static string GetString(string key) {
+        ensureInitialization();
+        return config.Value<string>(key);
+    }
+    public static int GetInt(string key) {
+        ensureInitialization();
+        return config.Value<int>(key);
+    }
+    public static double GetDouble(string key) {
+        ensureInitialization();
+        return config.Value<double>(key);
+    }
+    public static float[] GetFloatArray(string key) {
+        ensureInitialization();
+        return config.Value<JArray>(key).ToObject<float[]>();
+    }
+}
