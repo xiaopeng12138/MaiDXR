bb3a51a607634f0a5deea9929e558d0e8b499512
xpeng <1216772231@qq.com>
2022-08-29 03:24:40 +0200

    fix debug button config constant write to file issue

--- Assets/Scripts/Networks/DebugButton.cs
+++ Assets/Scripts/Networks/DebugButton.cs
@@ -5,8 +5,9 @@ public class DebugButton : MonoBehaviour
 {
     private void OnGUI() {
         GUILayout.BeginArea(new Rect(10, 10, 100, 300));
-        if (JsonConfig.HasKey("MultiplayerDebugButton") && JsonConfig.GetBoolean("MultiplayerDebugButton")) 
+        if (JsonConfig.HasKey("MultiplayerDebugButton")) 
         {
+            if (!JsonConfig.GetBoolean("MultiplayerDebugButton")) return;
                 if (GUILayout.Button("Host")) GetComponent<StartManager>().StartHost();
                 if (GUILayout.Button("Client")) GetComponent<StartManager>().StartClient();
                 if (GUILayout.Button("Stop")) GetComponent<StartManager>().StopAll();
